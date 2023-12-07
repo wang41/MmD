ca7799c73e9d5acdf484d2f25f6f89e96245565a
wang41 <wang41@163.com>
2023-12-07 23:41:56 +0800

    Add files via upload

---
+++ alg1.r
@@ -0,0 +1,60 @@
+library(NMOF)
+
+source('criteria_functions.R')
+
+Mm.Utype.mirror.cyc <- function(m, init = NULL, nS=1000, nT=50){
+  
+  x0 <- init
+  
+  neighbour <- function(x1){
+    
+    return(sample(x1,m)*sample(c(-1,1),m,replace=TRUE))
+    
+  }
+  
+  
+  ## *Threshold Accepting*
+  
+  algo <- list()
+  
+  algo$nT <- nT
+  
+  algo$nS <- nS
+  
+  algo$x0 <- x0
+  
+  algo$neighbour <- neighbour
+  
+  OF<-d2.minus.cyc
+  
+  sol <- TAopt(OF, algo = algo)
+  
+  bestsol <- sol
+  
+  xf1=bestsol$xbest
+  
+  best.criterion = bestsol$OFvalue
+  
+  return(list(x=xf1, criterion=best.criterion))
+  
+}
+
+# generated design for s = 5, m = 15 for example
+
+s <- 5
+
+m <- 15
+
+xx <- sample(rep((-(s-1)/2):((s-1)/2),m/s))
+
+res <- Mm.Utype.mirror.cyc(m=m, init = xx)
+
+xxopt = res$x
+
+Xopt = cyc(xxopt)
+
+Dopt <- rbind(Xopt,-Xopt)
+
+Dopt
+
+d2(Dopt)
---
+++ criteria_functions.r
@@ -0,0 +1,24 @@
+cyc <- function(x){
+  n <- length(x)
+  A <- diag(n)
+  A[1,] <- x
+  for(i in 2:n) A[i,] <- c(A[i-1,-1],A[i-1,1])
+  A
+}
+
+offdiag.max.cyc <- function(x){
+  X <- cyc(x)
+  n <- length(x)
+  max(abs(t(X[1,]) %*% t(X[2:n,])))
+}
+
+
+d2 <- function(D){
+  min(dist(D))^2
+}
+
+d2.minus.cyc <- function(x){
+  X <- cyc(x)
+  D = rbind(X,-X)
+  -d2(D)
+}
